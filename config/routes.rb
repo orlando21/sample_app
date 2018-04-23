14fd182a712dd65d4b929fcb7b04859570fb2235
orlando21 <me@gregbabb.com>
2018-04-23 03:52:54 +0200

    Made contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+
+  get 'foo/baz'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
+
+  get  'static_pages/about'
+
+  get  'static_pages/contact'
 
   root 'application#hello'
 end
