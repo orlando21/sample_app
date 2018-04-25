19fdb1614401482be5f892af6df49bf53b1dc183
orlando21 <me@gregbabb.com>
2018-04-25 16:04:03 +0200

    Add uniqueness validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425084652) do
+ActiveRecord::Schema.define(version: 20180425132805) do
 
   create_table "Test", id: false, force: :cascade do |t|
     t.text "model"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180425084652) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
