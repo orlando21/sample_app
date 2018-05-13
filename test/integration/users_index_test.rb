dd60a2ed3c8eeb2c26cada6fb349bf6ef030e634
orlando21 <me@gregbabb.com>
2018-05-13 16:41:16 +0200

    Activation test and refactoring

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -12,11 +12,15 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
-    first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
+    User.paginate(page: 1).each do |user|
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), false
       end
     end
     assert_difference 'User.count', -1 do
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -68,6 +68,10 @@ foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
+  end
+
 
 
 end
