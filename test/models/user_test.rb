e99dd8ff1d4545329aac45459750ce3f3b670a88
orlando21 <me@gregbabb.com>
2018-05-17 16:42:39 +0200

    Move logged_in filter to ApplicationsController

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -72,6 +72,12 @@ foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     assert_not @user.authenticated?(:remember, '')
   end
 
-
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 
 end
